324028602fda5c0cbb07da2aa3409a50f8c37bdf
ChkKris <kristina141098@gmail.com>
2023-12-19 21:14:32 +0200

    Sorting an array of numbers

---
+++ app/js/script.js
@@ -0,0 +1,12 @@
+// Sorting an array of numbers
+const numbers = [3, 5, 6, 4, 1, 2];
+for (let i = 0; i < numbers.length - 1; i++) {
+	for (let j = i + 1; j < numbers.length; j++) {
+		if (numbers[i] > numbers[j]) {
+			const temp = numbers[j];
+			numbers[j] = numbers[i];
+			numbers[i] = temp;
+		}
+	}
+}
+console.log("numbers:", numbers);
